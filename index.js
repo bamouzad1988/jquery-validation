85b2199bdabfbbe8d7228241d68ab72873a98930
Bijan <74878527+bamouzad1988@users.noreply.github.com>
2023-02-04 22:15:56 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ $(document).ready(function () {
     alert("ddd");
   };
   $("#commentForm").validate({
+    validClass: "success",
     submitHandler: () => {
       aaa();
+    },
+    rules: {
+      name: {
+        required: true,
+        minlength: 3,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "لطفا نام خود را به درستی وارد کنید",
+        minlength: "حداقل طول نام 3 حزف می باشد",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
     },
   });
 });
